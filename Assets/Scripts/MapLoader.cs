9786dfc5eea4a5a98e695cded5098062e12c896b
Veltouille <julienstatho@orange.fr>
2019-06-23 22:28:09 +0200

    MapLoaderScript

---
+++ Assets/Scripts/MapLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLoader : MonoBehaviour
+{
+    GameObject playTile;
+    GameObject[] borderTiles;
+    GameObject[] outerTiles;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
