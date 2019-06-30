93de8f7479951e6afce5b401f60e9e348a0c9cb0
Myrilandel <jonathan.navarro.amu@gmail.com>
2019-06-30 12:20:58 +0200

    New SelectorService
    +Adds the possibility to select a living entity
    +Adds the possibility to select a tile map
    +Adds a new SelectorController that handles the entity which is currently selected

---
+++ Assets/Scripts/SelectorService/LivingEntitySelectable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SelectorService
+{
+    public class LivingEntitySelectable : SelectableImpl
+    {
+        public override void MouseOver()
+        {
+            if (IsSelectable)
+            {
+                Debug.Log("I am a living Entity ! If you click on me, i will display some info !");
+            }
+            else
+            {
+                Debug.Log("I am a living Entity ! But i am not selectable yet ! If you click on my, nothing will happen !");
+            }
+        }
+
+        public override void OnClick()
+        {
+            Debug.Log("You clicked on me ! Now you can choos to attack or heal me !");
+        }
+    }
+}
---
+++ Assets/Scripts/SelectorService/TileEntitySelectable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SelectorService
+{
+    public class TileEntitySelectable : SelectableImpl
+    {
+        public override void MouseOver()
+        {
+            if (IsSelectable)
+            {
+                Debug.Log("I am a tile Entity ! I should display at this point if you can move to me or not !");
+            }
+            else
+            {
+                Debug.Log("I am a tile Entity ! But i am not selectable yet ! If you click on my, nothing will happen !");
+            }
+        }
+
+        public override void OnClick()
+        {
+            Debug.Log("You clicked on me ! If you have enough move points and if a path is available, i shoudl tell you how to come to me. If, you don't i should display and error message on your screen.");
+        }
+    }
+}
