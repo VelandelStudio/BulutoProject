55957886be5894533e5796357a0ef8ccfe4ebe70
Veltouille <julienstatho@orange.fr>
2019-07-02 23:36:55 +0200

    Moving Camera

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    private float speed = 80f;
+
+    private void LateUpdate()
+    {
+        Vector3 newPos;
+
+        float clampCameraZ;
+        float xAxisValue = Input.GetAxis("Horizontal") * speed;
+
+        clampCameraZ = Mathf.Clamp(transform.position.z + (xAxisValue * Time.deltaTime), -70, 80);
+        newPos = new Vector3(transform.position.x, transform.position.y, clampCameraZ);
+
+        transform.position = newPos;
+    }
+}
